3ae5cf3ed5eb547b7570426f3256e72ad803518c
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-20 03:11:53 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -9,7 +9,14 @@ class RepositoriesController < ApplicationController
     req.params['client_id'] = 'Iv1.84bad2a8c2101153'
     req.params['client_secret'] = 'f87cf4ba5c1bb333ff649aa545d427e24432dc1e'
     req.params['q'] = params[:query]
-
     end
+    body = JSON.parse(@resp.body)
+     if @resp.success?
+       @repositories = body["items"]
+     else
+       @error = body["message"]
+     end
+     render 'search'
+  
   end
 end
