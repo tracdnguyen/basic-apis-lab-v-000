6402b31eb04483aacd2cc3489096cf7516529816
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-20 03:10:40 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,12 +5,10 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
-    @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
     req.params['client_id'] = 'Iv1.84bad2a8c2101153'
     req.params['client_secret'] = 'f87cf4ba5c1bb333ff649aa545d427e24432dc1e'
-    req.params['v'] = '20160201'
-    req.params['near'] = params[:zipcode]
-    req.params['query'] = 'coffee shop'
+    req.params['q'] = params[:query]
 
     end
   end
